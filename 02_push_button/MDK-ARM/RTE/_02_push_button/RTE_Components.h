c98bc65a8026c9d1c03fe3a4e8f3627a20a6a9c5
cheavorn <cheavornh@gmail.com>
2019-02-24 15:32:11 +0700

    push button with debug variable value

---
+++ 02_push_button/MDK-ARM/RTE/_02_push_button/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '02_push_button' 
+ * Target:  '02_push_button' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
